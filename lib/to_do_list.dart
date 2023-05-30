46cae8fd99b49a16f8d412862b04049c3c7b23d8
TakaFriman <geor.tebiev@mail.ru>
2023-05-30 21:08:09 +0300

    begin

---
+++ lib/to_do_list.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ToDoList extends StatelessWidget {
+  const ToDoList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Column(
+        children: [
+          Center(
+            child: Text('fgf'),
+          )
+        ],
+      ),
+    );
+  }
+}
